834fa0ae2c665372997bc2e40776cac8eae5335e
Tomek Diakow <tomek.diakow@gmail.com>
2018-05-04 22:45:04 +0200

    Add docker

--- html/src/main.js
+++ html/src/main.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component{
 	render(){
 		return (
-            <h1>React -  Django rest starter !</h1>
+            <h1>React - Django rest starter !</h1>
 		)
 	}
 }
